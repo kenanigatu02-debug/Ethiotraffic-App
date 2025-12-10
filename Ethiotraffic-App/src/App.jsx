669490510b99b00ba7e9c7498e4d4a99e177eab9
@kena12 <kenanigatu02@gmail.com>
2025-12-09 23:09:04 -0800

    Initial commit: Ethiotraffic-App

---
+++ Ethiotraffic-App/src/App.jsx
@@ -0,0 +1,24 @@
+
+function App(){
+return (
+   <div>
+    <h1>road block and accident reporter(addistraffic ET)</h1>
+    <h2>about this project </h2>
+    <p> 
+     this website helps people report and see road blocks,accidents,
+     and traffic problems happening in addis ababa.
+    </p>
+    <h2>what you can do?</h2>
+    <ul>
+     <li>report an accident</li>
+      <li>view recent traffic issues</li>
+     </ul>
+    <h2>goal</h2>
+    <p>
+make traveling safer and faster by sharing real time road information.
+    </p>
+    </div>
+);
+
+}
+export default(App);
